e917ce4915fcb37c5530f80d98f034758866ef5f
Nikita <justnicke@gmail.com>
2020-12-10 20:21:19 +0300

    Initial Commit

---
+++ TestTaskFS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskFS
+//
+//  Created by Nikita Sukachev on 10.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
