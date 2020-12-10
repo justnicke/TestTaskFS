3bbabee08d00e315cd2a32fb81f64c8a94bcc03d
Nikita <justnicke@gmail.com>
2020-12-10 21:26:09 +0300

    Setup AlbumViewController

--- TestTaskFS/Views/AlbumViewController.swift
+++ TestTaskFS/Views/AlbumViewController.swift
@@ -4,14 +4,86 @@
 //
 //  Created by Nikita Sukachev on 10.12.2020.
 //
-
 import UIKit
 
 final class AlbumViewController: UIViewController {
-
+    
+    // MARK: - Public Properties
+    
+    // MARK: - Private Properties
+    
+    private var collectionView: UICollectionView!
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var albums: [Album] = []
+    
+    // MARK: - Public Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .red
+        
+        
+        setupCollectionView()
+        setupSearchController()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = setupLayout()
+        collectionView.register(AlbumCell.self, forCellWithReuseIdentifier: AlbumCell.reuseId)
+    }
+    
+    private func setupLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { _, _ -> NSCollectionLayoutSection? in
+            let layoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(120))
+            let item = NSCollectionLayoutItem(layoutSize: layoutSize)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: layoutSize, subitem: item, count: 3)
+            group.interItemSpacing = NSCollectionLayoutSpacing.fixed(16.0)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 16
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            
+            return section
+        }
+    }
+    
+    private func setupSearchController() {
+        definesPresentationContext = true
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
     }
 }
+
+// MARK: - CollectionViewDataSource & CollectionViewDelegate
+
+extension AlbumViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AlbumCell.reuseId, for: indexPath) as! AlbumCell
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: - SearchBarDelegate
+
+extension AlbumViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    }
+}
+
